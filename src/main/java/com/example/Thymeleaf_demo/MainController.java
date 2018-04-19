ef1d490defba27974e92ffffc1164e10edb3ae6a
nOy39 <popov.a39rus@gmail.com>
2018-04-19 14:48:05 +0300

    nOy39
    FirstCommit

---
+++ src/main/java/com/example/Thymeleaf_demo/MainController.java
@@ -0,0 +1,106 @@
+package com.example.Thymeleaf_demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by @author OGI aka nOy39
+ *
+ * @Date 17.04.2018
+ * @Time 11:02
+ */
+
+@Controller
+public class MainController {
+
+    private static List<Person> persons = new ArrayList<Person>();
+
+   private UserForm user = new UserForm();
+
+    static {
+        persons.add(new Person("Bill", "Gates"));
+        persons.add(new Person("Steve", "Jobs"));
+    }
+
+    // ​​​​​​​
+    // Вводится (inject) из application.properties.
+    @Value("${welcome.message}")
+    private String message;
+
+    @Value("${error.message}")
+    private String errorMessage;
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("message", message);
+
+        return "index";
+    }
+
+    @RequestMapping(value = { "/personList" }, method = RequestMethod.GET)
+    public String personList(Model model) {
+
+        model.addAttribute("persons", persons);
+
+        return "personList";
+    }
+
+    @RequestMapping(value = { "/addPerson" }, method = RequestMethod.GET)
+    public String showAddPersonPage(Model model) {
+
+        PersonForm personForm = new PersonForm();
+        model.addAttribute("personForm", personForm);
+
+        return "addPerson";
+    }
+
+    @RequestMapping(value = { "/addPerson" }, method = RequestMethod.POST)
+    public String savePerson(Model model, //
+                             @ModelAttribute("personForm") PersonForm personForm) {
+
+        String firstName = personForm.getFirstName();
+        String lastName = personForm.getLastName();
+
+        if (firstName != null && firstName.length() > 0 //
+                && lastName != null && lastName.length() > 0) {
+            Person newPerson = new Person(firstName, lastName);
+            persons.add(newPerson);
+
+            return "redirect:/personList";
+        }
+
+        model.addAttribute("errorMessage", errorMessage);
+        return "addPerson";
+    }
+
+    @RequestMapping(value = {"/login"}, method = RequestMethod.POST)
+    public String loginUser(Model model, //
+                            @ModelAttribute("userForm") UserForm userForm) {
+
+        String login = userForm.getLogin();
+        String password = userForm.getPassword();
+
+        System.out.println(login+" "+password);
+
+
+        return "login";
+    }
+
+
+    @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
+    public String showLoginPage(Model model) {
+
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
+
+        return "login";
+    }
+}
---
+++ src/main/java/com/example/Thymeleaf_demo/UserForm.java
@@ -0,0 +1,30 @@
+package com.example.Thymeleaf_demo;
+
+/**
+ * Created by @author OGI aka nOy39
+ *
+ * @Date 17.04.2018
+ * @Time 13:32
+ */
+public class UserForm {
+
+    private String login;
+    private String password;
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
